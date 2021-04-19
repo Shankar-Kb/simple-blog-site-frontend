18bc23eed476bca95f085222046c187eef28d1c3
Shankar-Kb <shankar.kb95@gmail.com>
2021-04-19 16:57:39 +0530

    No changes

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from 'react-router-dom';
-//import { connect } from 'react-redux';
 import { useSelector, useDispatch } from 'react-redux'
 
 const LoginPage = () => {
